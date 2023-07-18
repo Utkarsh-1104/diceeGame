deffddb12f584da311ac7213c26a0ce460425129
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2023-07-18 21:56:12 +0530

    .

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function roll() {
     let randomNum1 = Math.floor((Math.random() * 6) + 1);
     let randomDice1 = "images/dice" + randomNum1 + ".png";
-    console.log(randomDice1);
     document.getElementById("img1").setAttribute("src", randomDice1);
 
     let randomNum2 = Math.floor((Math.random() * 6) + 1);
     let randomDice2 = "images/dice" + randomNum2 + ".png";
-    console.log(randomDice2);
     document.getElementById("img2").setAttribute("src", randomDice2);
 
     if (randomNum1 > randomNum2) {
